58dc29ceed611c84c5f9f13db752a9aa433d97fa
Frank Nasseri <franknasseri57@gmail.com>
2017-07-07 13:18:11 -0400

    practice with randoms

--- RandomPractice/Program.cs
+++ RandomPractice/Program.cs
@@ -10,17 +10,23 @@ namespace RandomPractice
     {
         static void Main(string[] args)
         {
-            //creating a random object
+            ////creating a random object
             Random randomNumber = new Random();
-            //saving the random number from our random object (randomNumber)
+            ////saving the random number from our random object (randomNumber)
             int randy = randomNumber.Next(0, 5);
-            //printing out the random int variable
-            Console.WriteLine(randy);
-            Console.WriteLine(randy);
-            Console.WriteLine(randy);
-            //did NOT save that variable to an integer
-            Console.WriteLine(randomNumber.Next(0, 9));
+            ////printing out the random int variable
+            //Console.WriteLine(randy);
+            //Console.WriteLine(randy);
+            //Console.WriteLine(randy);
+            ////did NOT save that variable to an integer
+            //Console.WriteLine(randomNumber.Next(0, 9));
 
+            string[] words = { "Paper", "Rock", "Scissors" };
+            int[] numbers = { 1, 2, 3, 76, 6 };
+            string sayings = "You can never have to much cake";
+            Console.WriteLine(words[randomNumber.Next(0, 3)]);
+            Console.WriteLine(numbers[randomNumber.Next(0, 5)]);
+            Console.WriteLine(sayings[randomNumber.Next(0, sayings.Length)]);
             
         }
     }
