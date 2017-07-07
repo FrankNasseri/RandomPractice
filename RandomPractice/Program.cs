21a8189ef9032878a126e8b0769ea31f4098c96b
Frank Nasseri <franknasseri57@gmail.com>
2017-07-07 11:57:40 -0400

    initial commit

---
+++ RandomPractice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating a random object
+            Random randomNumber = new Random();
+            //saving the random number from our random object (randomNumber)
+            int randy = randomNumber.Next(0, 5);
+            //printing out the random int variable
+            Console.WriteLine(randy);
+            Console.WriteLine(randy);
+            Console.WriteLine(randy);
+            //did NOT save that variable to an integer
+            Console.WriteLine(randomNumber.Next(0, 9));
+
+            
+        }
+    }
+}
